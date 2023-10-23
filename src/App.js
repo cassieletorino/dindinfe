78ed90f6d59e08ecceabfae1fe875b14b493b87b
cassieletorino <ketlin.cassi121@gmail.com>
2023-10-23 11:27:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+let pessoas = [
+  { COD: "001", NOME: "Aimée" },
+  { COD: "002", NOME: "Cassi" },
+  { COD: "003", NOME: "Malluh" },
+];
+
+function MyTitle() {
+  return <h1>DinDin</h1>;
+}
+
+function MyTable() {
+  return (
+    <table>
+      <tr>
+        <td>COD</td>
+        <td>NOME</td>
+      </tr>
+      {pessoas.map((Pessoa) => (
+        <tr>
+          <td>{Pessoa.COD}</td>
+          <td>{Pessoa.NOME}</td>
+        </tr>
+      ))}
+    </table>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyTitle />
+        <MyTable />
       </header>
     </div>
   );
